6f0d4a11d26025f20dae971577ea0f6c4f9df1e0
deepblue6 <joerosyleona@gmail.com>
2025-03-10 16:02:41 -0700

    Video 17

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,16 @@
+let js = "amazing";
+console.log(40 + 8 + 23 - 10);
+console.log(typeof js);
+console.log(typeof null);
+
+let age = 30;
+var job = "teacher";
+const year = 2025;
+// why not have every variable be let?
+// let variables can be changed, but const variables cannot be changed
+// var is function scoped
+// let and const are block scoped
+
+/* let variables can be changed, but const variables cannot be changed */
+const isIsland = false;
+let language;
