9a7ad7df494bf3fc555e0f3b0da08260e2569487
deepblue6 <joerosyleona@gmail.com>
2025-03-19 13:54:42 -0700

    On video #39

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,16 +1,41 @@
-let js = "amazing";
-console.log(40 + 8 + 23 - 10);
-console.log(typeof js);
-console.log(typeof null);
+// let js = "amazing";
+// console.log(40 + 8 + 23 - 10);
+// console.log(typeof js);
+// console.log(typeof null);
 
-let age = 30;
-var job = "teacher";
-const year = 2025;
+// let age = 30;
+// var job = "teacher";
+// const year = 2025;
 // why not have every variable be let?
 // let variables can be changed, but const variables cannot be changed
 // var is function scoped
 // let and const are block scoped
 
 /* let variables can be changed, but const variables cannot be changed */
-const isIsland = false;
-let language;
+// const isIsland = false;
+// let language;
+// console.log(
+//   "multiple line \n\
+// string \n\
+// f"
+// );
+// console.log(Number("Jonas"), +2 + 23);
+
+// // 5 falsy values: 0, '', null, undefined, NaN, false
+// if (0) console.log("not executed");
+// if (null) console.log("not executed");
+// if (undefined) console.log("not executed");
+// if (NaN) console.log("not executed");
+// if (false) console.log("not executed");
+
+// const money = 0;
+// if (money) {
+//   // 0, '', null, undefined, NaN, false are all falsy values
+//   console.log("Don't spend it all");
+// } else console.log("You should get a job!");
+
+// const equation = 1 ** 1 + 2 === 1;
+// console.log(Boolean(3));
+
+const age = 23;
+age >= 18 ? console.log("Can drive") : console.log("Cannot drive");
---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,14 @@
+"use strict";
+
+// functions video #33
+// stopped on video 39
+
+function add(a, b) {
+  return a + b;
+}
+
+const d = function (z) {
+  console.log("yay");
+};
+
+d();
